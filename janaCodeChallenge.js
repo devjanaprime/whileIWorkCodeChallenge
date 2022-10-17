ca22df5400cfa441ec8d89021e6c8ddd7a12b21e
devjanaprime <devjana@yahoo.com>
2022-10-17 14:06:13 -0500

    invalid shifts flagged

--- janaCodeChallenge.js
+++ janaCodeChallenge.js
@@ -1,9 +1,11 @@
-const data = require( './data/dataset.json' );
+const data = require( './data/datasmall.json' );
 const verbose = true;
+const testing = true;
 
 let sortEmployees = ( data ) =>{
     let employees = [];
     let employeeIDs = [];
+    // sort by employee
     for( record of data ){
         if( employeeIDs.includes( record.EmployeeID ) ){
             if( verbose ) console.log( 'repeat employeeID:', record ); 
@@ -18,29 +20,29 @@ let sortEmployees = ( data ) =>{
             });
         } // end new employee
     }
-    console.log( 'employeeIDs:', employeeIDs );
-    console.log( 'employees:', employees );
-    console.log( employees[0].shifts[0].EndTime, employees[0].shifts[0].StartTime );
-    /// - tests - ///
+    // flag invalid shifts
     for( employee of employees ){
-        filterInvalidShifts( employee );
+        employee = flagInvalidShifts( employee );
+    }
+    // testing
+    if( testing ) {
+        console.log( 'employeeIDs:', employeeIDs );
+        console.log( 'employees:', employees );
+        for( employee of employees ){
+            for( shift of employee.shifts ){
+                if( shift.invalid ){
+                    console.log( shift );
+                }
+            }
+        }
     }
 }
 
-let convertDate = ( date ) => {
-    if( verbose ) console.log( 'in convertDate:', date );
-    let calendarDate = date.split( 'T' )[0];
-    let time = date.split( 'T' )[1].split( 'Z' )[0];
-    return {
-        calendarDate: calendarDate,
-        day: new Date( date ).getDay(),
-        time: time
-    }
-}
 
-let filterInvalidShifts = ( employee ) => {
+
+let flagInvalidShifts = ( employee ) => {
     // if( verbose ) console.log( 'in filterInvalidShifts:', employee );
-    remove = [];
+    invalidShifts = [];
     for( let i=0; i< employee.shifts.length; i++  ){
         let shiftAStart = Date.parse( employee.shifts[ i ].StartTime );
         let shiftAEnd = Date.parse( employee.shifts[ i ].EndTime );
@@ -50,24 +52,39 @@ let filterInvalidShifts = ( employee ) => {
             let dupeCheckEnd = Date.parse( employee.shifts[ j ].EndTime );
             if( max(shiftAEnd, dupeCheckEnd) - min(shiftAStart, dupeCheckStart) < (shiftAEnd - shiftAStart) + (dupeCheckEnd - dupeCheckStart) ){
                 console.log( 'dupe!', employee.shifts[ i ], employee.shifts [ j ] );
-                remove.push( employee.shifts[ i ] );
-                remove.push( employee.shifts[ j ] );
+                invalidShifts.push( employee.shifts[ i ].invalid=true );
+                invalidShifts.push( employee.shifts[ j ].invalid=true );
             }
         }
     }
-    if( verbose && remove.length > 0 ) console.log( 'removing invalid shifts:', remove, 'from employee:', employee );
-}
+    return employee;
+} // end filterInvaid
 
+
+// utils
+let convertDate = ( date ) => {
+    if( verbose ) console.log( 'in convertDate:', date );
+    let calendarDate = date.split( 'T' )[0];
+    let time = date.split( 'T' )[1].split( 'Z' )[0];
+    return {
+        calendarDate: calendarDate,
+        day: new Date( date ).getDay(),
+        time: time
+    }
+} //end convert date
 let max = ( a, b )=>{
     if( a > b ){
         return a;
     } 
     return b;
-}
+} // end max
 let min = ( a, b )=>{
     if( a < b ){
         return a;
     } 
     return b;
-}
+} // end min
+
+
+// init
 sortEmployees( data );
