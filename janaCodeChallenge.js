81866e717a7a48a8087ac04f1a9daa24d030cc1d
devjanaprime <devjana@yahoo.com>
2022-10-17 12:24:02 -0500

    sorting by employees

--- janaCodeChallenge.js
+++ janaCodeChallenge.js
@@ -1,2 +1,25 @@
-const data = require( './data/dataset.json' );
-console.log( data );
+const data = require( './data/datasmall.json' );
+const verbose = true;
+
+let sortEmployees = ( data ) =>{
+    let employees = [];
+    let employeeIDs = [];
+    for( record of data ){
+        if( employeeIDs.includes( record.EmployeeID ) ){
+            if( verbose ) console.log( 'repeat employeeID:', record ); 
+            employees[ employeeIDs.indexOf( record.EmployeeID ) ].shifts.push( record );
+        } // end new employee
+        else{
+            if( verbose ) console.log( 'new employeeID' ); 
+            employeeIDs.push( record.EmployeeID );
+            employees.push( {
+                EmployeeID: record.EmployeeID,
+                shifts: [ record ]
+            });
+        } // end new employee
+    }
+    console.log( 'employeeIDs:', employeeIDs );
+    console.log( 'employees:', employees );
+}
+
+sortEmployees( data );
